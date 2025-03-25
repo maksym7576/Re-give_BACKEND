4ee825b423af46cbaa5000efe24723e4440b290a
Maksym7576 <ukrainemaksymhryshchuk@gmail.com>
2025-03-25 10:36:40 +0100

    delete login and register

--- functions/src/controllers/userController.js
+++ functions/src/controllers/userController.js
@@ -4,43 +4,6 @@ const jwt = require('jsonwebtoken'); // Ensure you have jwt installed and requir
 
 const JWT_SECRET = '324324324234324234'; // Replace with your actual secret key
 
-async function registerUser(req, res) {
-    try {
-        const { email, password } = req.body;
-        const userRecord = await admin.auth().createUser({ email, password });
-        await setUserRole(userRecord.uid, 'user');
-        const payload = {
-            uid: userRecord.uid,
-            email: userRecord.email,
-            role: 'user'
-        };
-        const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '30d' });
-        res.status(200).json({ token, uid: userRecord.uid, role: 'user' });
-    } catch (error) {
-        console.error("Error ", error);
-        res.status(500).json({ error: error.message });
-    }
-}
-
-async function loginUser(req, res) {
-    try {
-        const { email, password } = req.body;
-        const userRecord = await admin.auth().getUserByEmail(email);
-        const role = (userRecord.customClaims && userRecord.customClaims.role) || 'user';
-        const payload = {
-            uid: userRecord.uid,
-            email: userRecord.email,
-            role: role
-        };
-        const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '30d' });
-        res.status(200).json({ token, uid: userRecord.uid, role: role });
-    } catch (error) {
-        console.error("Error ", error);
-        res.status(500).json({ error: error.message });
-    }
-}
-
-
 async function listOfUsersWithRole(req, res) {
     try {
         const listUsersResult = await admin.auth().listUsers();
@@ -58,4 +21,4 @@ async function listOfUsersWithRole(req, res) {
     }
 }
 
-module.exports = { registerUser, loginUser, listOfUsersWithRole  };
+module.exports = { listOfUsersWithRole  };
--- functions/src/routes/userRoutes.js
+++ functions/src/routes/userRoutes.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser, listOfUsersWithRole } = require('../controllers/userController');
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
 router.get('/users/roles', listOfUsersWithRole);
 
 module.exports = router;
